0234967163dc6c114cd88d2a8fdd3e13b1f19205
Bill Laboon <laboon@gmail.com>
2014-04-30 01:03:46 +0000

    Using Integer instead of int for arbitrarily large values

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -2,21 +2,8 @@ module Handler.Home where
 
 import Import
 
-{-
-
-Yesod follows a naming convention for handler function names: the lower-cased
-HTTP request method, followed by the route name. Therefore, the function for
-HomeR's GET handler would be getHomeR.
-
-Each handler function lives in the Handler monad, and has to return a value
-that can be serialized over an HTTP connection. Two common examples of such
-values are HTML and JSON data. In this case, we'll return Html.
-
--}
 getHomeR :: Handler Html
--- defaultLayout uses the application's standard page layout to display
--- some contents. In our application, we're just using the standard
--- layout, which includes a basic HTML 5 page outline.
+
 getHomeR = defaultLayout $ do
 
     setTitle "Triangular Number"
--- src/Handler/Triangle.hs
+++ src/Handler/Triangle.hs
@@ -2,16 +2,16 @@ module Handler.Triangle where
 
 import Import
 
--- An infinite list of triangular numbers
-triangles :: Int -> Integer
-triangles n = foldl (+) 0 $ take n [1..]
+-- Given an integer n, return the nth triangular number
+triangles :: Integer -> Integer
+triangles n = sum $ take (fromIntegral n) [1..]
 
 -- An error message
-getError :: [Char]
+getError :: String
 getError = "Error"
 
 -- Result is a JSON object, with key = "triangular" and value = "value"
-getTriR :: Int -> Handler Value
+getTriR :: Integer -> Handler Value
 getTriR n
-    | n < 1     = return $ object ["triangular" .= (getError)]
-    | otherwise = return $ object ["triangular" .= (triangles (abs n))]
+    | n < 1     = return $ object ["triangular" .= getError]
+    | otherwise = return $ object ["triangular" .= triangles (abs n)]
