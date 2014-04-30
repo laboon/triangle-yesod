d6260f04687df4d8b5d87e9a469f89f35bf21bbc
Bill Laboon <laboon@gmail.com>
2014-04-30 01:04:30 +0000

    Changed name of triangles to triangle

--- src/Handler/Triangle.hs
+++ src/Handler/Triangle.hs
@@ -3,8 +3,8 @@ module Handler.Triangle where
 import Import
 
 -- Given an integer n, return the nth triangular number
-triangles :: Integer -> Integer
-triangles n = sum $ take (fromIntegral n) [1..]
+triangle :: Integer -> Integer
+triangle n = sum $ take (fromIntegral n) [1..]
 
 -- An error message
 getError :: String
@@ -14,4 +14,4 @@ getError = "Error"
 getTriR :: Integer -> Handler Value
 getTriR n
     | n < 1     = return $ object ["triangular" .= getError]
-    | otherwise = return $ object ["triangular" .= triangles (abs n)]
+    | otherwise = return $ object ["triangular" .= triangle (abs n)]
